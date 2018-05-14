d0878448aa1af2bf4ba3449227aaf6a267fe3ae9
Matt Lauer <matt.lauer@detroitlabs.com>
2018-05-14 09:54:06 -0400

    Add functions to add and remove entries from the doubly linked list

--- programming-c/ch10-doubly-linked-list.c
+++ programming-c/ch10-doubly-linked-list.c
@@ -9,6 +9,19 @@ struct entry
   struct entry  *next;
 };
 
+void insertEntry (struct entry *startingEntryPtr, struct entry *newEntryPtr)
+{
+  newEntryPtr->previous = startingEntryPtr;
+  newEntryPtr->next = startingEntryPtr->next;
+  startingEntryPtr->next = newEntryPtr;
+}
+
+void removeEntry (struct entry *entryPtr)
+{
+  entryPtr->previous->next = entryPtr->next;
+  entryPtr->next->previous = entryPtr->previous;
+}
+
 int main (void)
 {
   struct entry n1, n2, n3;
@@ -26,9 +39,33 @@ int main (void)
   n3.previous = &n2;
   n3.next = (struct entry *) 0;
 
+  printf ("The original doubly linked list!\n");
   while ( list_pointer != (struct entry *) 0 )
   {
     printf("%i\n", list_pointer->value);
     list_pointer = list_pointer->previous;
+  }
+
+  struct entry n4;
+  n4.value = 250;
+  struct entry *insertPtr = &n2;
+  printf ("\nNow let's insert a new entry of value %i after value %i\n", n4.value, insertPtr->value);
+  insertEntry (insertPtr, &n4);
+  list_pointer = &n1;
+
+  while ( list_pointer != (struct entry *) 0 )
+  {
+    printf("%i\n", list_pointer->value);
+    list_pointer = list_pointer->next;
+  }
+
+  printf ("\nNow let's remove that new entry and show the list again\n");
+  removeEntry (&n4);
+  list_pointer = &n1;
+
+  while ( list_pointer != (struct entry *) 0 )
+  {
+    printf("%i\n", list_pointer->value);
+    list_pointer = list_pointer->next;
   }
 }
