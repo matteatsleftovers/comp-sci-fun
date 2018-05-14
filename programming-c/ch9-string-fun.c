3881d95afae75faed319f21599df03a0a7a517e2
Matt Lauer <matt.lauer@detroitlabs.com>
2018-05-14 07:59:31 -0400

    Add removeString function for ch9

--- programming-c/ch9-string-fun.c
+++ programming-c/ch9-string-fun.c
@@ -7,7 +7,7 @@
 int getStringSize (const char *s1)
 {
   int size = 0;
-  while (s1[size] != '\0') size++;
+  while ( s1[size] != '\0' ) size++;
   return size;
 }
 
@@ -18,10 +18,10 @@ int findString (const char s1[], const char s2[])
   int s2Size = getStringSize (s2);
   int i = 0, j = 0;
 
-  while ( s1[i] != '\0')
+  while ( s1[i] != '\0' )
   {
     if (s1[i] == s2[j]) {
-      while (s1[i+j] == s2[j] && s2[j] != '\0') j++;
+      while ( s1[i+j] == s2[j] && s2[j] != '\0' ) j++;
       if (j == s2Size) return i;
       else j = 0;
     }
@@ -31,19 +31,42 @@ int findString (const char s1[], const char s2[])
   return -1;
 }
 
+void removeString (char text[], int startingIndex, int stringSize)
+{
+  int i = 0;
+  while ( text[i] != '\0' )
+  {
+    if ( i >= startingIndex )
+    {
+      text[i] = text[i + stringSize];
+    }
+    i++;
+  }
+}
+
 int main (void)
 {
-  int index;
-  char word[20], substring[20];
+  int startingIndex, stringSize;
+  char charString[20];
+  // , substring[20];
 
-  printf ("Enter a word: ");
-  scanf ("%19s", word);
-  printf ("Enter a word you want to find within: ");
-  scanf ("%19s", substring);
+  printf ("Enter a character string: ");
+  scanf ("%[^\n]", charString);
+  printf ("Great! I have that the string is \"%s\".\n", charString);
+  printf ("Enter the starting index for removing characters: ");
+  scanf("%i", &startingIndex);
+  printf ("Enter the length of the character string you want to remove: ");
+  scanf("%i", &stringSize);
+  // printf ("Enter a word you want to find within: ");
+  // scanf ("%19s", substring);
+  //
+  // index = findString (word, substring);
+  //
+  // printf ("\nLooks like your substring is at index %i.\n", index);
 
-  index = findString (word, substring);
+  removeString (charString, startingIndex, stringSize);
 
-  printf ("\nLooks like your substring is at index %i.\n", index);
+  printf ("\nThe altered string is now \"%s\".\n", charString);
 
   return 0;
 }
