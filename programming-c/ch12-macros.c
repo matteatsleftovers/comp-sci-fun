1c7ca6819ef349849b78827ec8cda4a5f384c458
Matt Lauer <matt.lauer@detroitlabs.com>
2018-06-03 21:09:23 -0400

    Add IS_DIGIT and IS_SPECIAL macros

--- programming-c/ch12-macros.c
+++ programming-c/ch12-macros.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 
-#define   IS_UPPER_CASE(x)  ( ((x) >= 'A') && (x) <= 'Z' )
-#define   IS_LOWER_CASE(x)  ( ((x) >= 'a') && (x) <= 'z' )
+#define   IS_UPPER_CASE(x)  ( ((x) >= 'A') && ((x) <= 'Z') )
+#define   IS_LOWER_CASE(x)  ( ((x) >= 'a') && ((x) <= 'z') )
 #define   IS_ALPHABETIC(x)  ( IS_UPPER_CASE(x) || IS_LOWER_CASE(x) )
+#define   IS_DIGIT(x)       ( ((x) >= '0') && ((x) <= '9') )
+#define   IS_SPECIAL(x)     ( !IS_ALPHABETIC(x) && !IS_DIGIT(x) )
 
 int main (void)
 {
   char c;
 
-  printf("Give me a character, please. I'll tell you if it is alphabetic.\n");
+  printf("Give me a character, please. I'll tell you if it is special.\n");
   scanf("%c", &c);
 
-  if (IS_ALPHABETIC(c)) {
-    printf("\nYeah, that's definitely alphabetic.\n");
+  if (IS_SPECIAL(c)) {
+    printf("\nYeah, that's definitely special, being non-alphabetic and non-digit.\n");
+  } else if (IS_ALPHABETIC(c)) {
+    printf("\nThat is not a special character. It's alphabetic.\n");
   } else {
-    printf("\nNG\n");
+    printf("\nThat is not a special character. It's a digit.\n");
   }
 
   return 0;
