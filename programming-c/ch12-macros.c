a62ad462e58342ba4a3f413198ba08ca34475988
Matt Lauer <matt.lauer@detroitlabs.com>
2018-06-03 21:02:45 -0400

    Add IS_LOWERCASE and IS_ALPHABETIC macros

--- programming-c/ch12-macros.c
+++ programming-c/ch12-macros.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 #define   IS_UPPER_CASE(x)  ( ((x) >= 'A') && (x) <= 'Z' )
+#define   IS_LOWER_CASE(x)  ( ((x) >= 'a') && (x) <= 'z' )
+#define   IS_ALPHABETIC(x)  ( IS_UPPER_CASE(x) || IS_LOWER_CASE(x) )
 
 int main (void)
 {
   char c;
 
-  printf("Give me a character, please. I'll tell you if it is uppercase.\n");
+  printf("Give me a character, please. I'll tell you if it is alphabetic.\n");
   scanf("%c", &c);
 
-  if (IS_UPPER_CASE(c)) {
-    printf("\nYeah, that's definitely uppercase.\n");
+  if (IS_ALPHABETIC(c)) {
+    printf("\nYeah, that's definitely alphabetic.\n");
   } else {
     printf("\nNG\n");
   }
