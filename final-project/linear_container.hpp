d52cb351bf1f9aad89157b87c04f7785fc2d4743
Matt Lauer <matt.lauer@detroitlabs.com>
2018-07-22 17:15:28 -0400

    Extract out common push and pop code

--- final-project/linear_container.hpp
+++ final-project/linear_container.hpp
@@ -1,6 +1,8 @@
 #ifndef LINEAR_CONTAINER_H
 #define LINEAR_CONTAINER_H
 
+#include <iostream>
+
 template <class T>
 struct listelem {
   T              data;
@@ -19,27 +21,54 @@ listelem<T>::listelem(T t, listelem<T>* prev, listelem<T>* next) {
 template <class T>
 class LinearContainer {
 public:
-  LinearContainer<T>();
+  LinearContainer<T>() { };
   LinearContainer<T>(int n);
-  virtual void push(T) = 0;
-  virtual T pop() = 0;
+  virtual void push(T t);
+  virtual T pop();
   bool is_empty();
   bool is_full();
 protected:
   listelem<T>* head = 0;
   listelem<T>* tail = 0;
 private:
-  int size;
+  int current_size = 0;
+  int max_size = 10;
 };
 
 template <class T>
-LinearContainer<T>::LinearContainer() {
-  this->size = 10;
+LinearContainer<T>::LinearContainer(const int n) {
+  this->max_size = n;
 }
 
 template <class T>
-LinearContainer<T>::LinearContainer(const int n) {
-  this->size = n;
+void LinearContainer<T>::push(T t) {
+  std::cout << "Trying to push!" << "\n";
+  std::cout << "The value of T is " << t << "\n";
+  if (this->is_full()) {
+    std::cout << "The container is full! Allocating more space..." << "\n";
+    this->max_size *= 2;
+    std::cout << "There are now " << max_size << " spaces allocated." << "\n";
+  }
+  if (this->head == 0 && this->tail == 0) { // Case of empty list
+    std::cout << "Initialize" << "\n";
+    listelem<T>* temp = new listelem<T>(t, 0, 0);
+    this->head = temp;
+    this->tail = temp;
+  } else {
+    std::cout << "Add new node" << "\n";
+    listelem<T>* temp = new listelem<T>(t, 0, this->head);
+    this->head->prev = temp;
+    this->head = temp;
+    std::cout << "The value of head is " << this->head->data << "\n";
+  }
+  std::cout << "\n";
+  current_size++;
+}
+
+template <class T>
+T LinearContainer<T>::pop() {
+  current_size--;
+  return 0;
 }
 
 template <class T>
@@ -49,8 +78,10 @@ bool LinearContainer<T>::is_empty() {
 
 template <class T>
 bool LinearContainer<T>::is_full() {
-  bool is_full = true;
-  return is_empty;
+  std::cout << "Checking if full..." << "\n";
+  std::cout << "current_size is " << current_size << "\n";
+  std::cout << "max_size is " << max_size << "\n";
+  return current_size == max_size;
 }
 
 #endif
--- final-project/queue.hpp
+++ final-project/queue.hpp
@@ -9,24 +9,12 @@ public:
 
 template <class T>
 void Queue<T>::push(T t) {
-  std::cout << "The value of T is " << t << "\n";
-  if (this->head == 0 && this->tail == 0) { // Case of empty list
-    std::cout << "Initialize queue" << "\n";
-    listelem<T>* temp = new listelem<T>(t, 0, 0);
-    this->head = temp;
-    this->tail = temp;
-  } else {
-    std::cout << "Add new node" << "\n";
-    listelem<T>* temp = new listelem<T>(t, 0, this->head);
-    this->head->prev = temp;
-    this->head = temp;
-    std::cout << "The value of head is " << this->head->data << "\n";
-  }
-  std::cout << "\n";
+  LinearContainer<T>::push(t);
 }
 
 template <class T>
 T Queue<T>::pop() {
+  LinearContainer<T>::pop();
   std::cout << "Trying to pop this queue!" << "\n";
   std::cout << "The value of head is " << this->head->data << "\n";
   std::cout << "The value of tail is " << this->tail->data << "\n";
