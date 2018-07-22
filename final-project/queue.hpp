52ab970e51a40b75136060c5b63fe99474107f18
Matt Lauer <matt.lauer@detroitlabs.com>
2018-07-22 10:32:38 -0400

    Introduce Queue and Stack classes

---
+++ final-project/queue.hpp
@@ -0,0 +1,5 @@
+#include "linear_container.hpp"
+
+template <class T>
+class Queue : public LinearContainer<T> {
+};
---
+++ final-project/stack.hpp
@@ -0,0 +1,5 @@
+#include "linear_container.hpp"
+
+template <class T>
+class Stack : public LinearContainer<T> {
+};
