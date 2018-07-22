c22197828f2bac7f744f9ac58955701c32094d2a
Matt Lauer <matt.lauer@detroitlabs.com>
2018-07-22 10:31:25 -0400

    Define is_empty and is_ful

--- final-project/linear_container.hpp
+++ final-project/linear_container.hpp
@@ -1,18 +1,46 @@
 template <class T>
 class LinearContainer {
 public:
-  LinearContainer();
-  LinearContainer(int n);
+  LinearContainer<T>();
+  LinearContainer<T>(int n);
   virtual void push(T);
   virtual T pop();
+  bool is_empty();
+  bool is_full();
 private:
   int size;
+  T* elements[10];
 };
 
-LinearContainer::LinearContainer() {
+template <class T>
+LinearContainer<T>::LinearContainer() {
   this->size = 10;
 }
 
-LinearContainer::LinearContainer(int n) {
+template <class T>
+LinearContainer<T>::LinearContainer(const int n) {
   this->size = n;
+  elements[size];
+}
+
+template <class T>
+bool LinearContainer<T>::is_empty() {
+  bool is_empty = true;
+  for (int i = 0; i < size; ++i) {
+    if (elements[i] != 0) {
+      is_empty = false;
+    }
+  }
+  return is_empty;
+}
+
+template <class T>
+bool LinearContainer<T>::is_full() {
+  bool is_full = true;
+  for (int i = 0; i < size; ++i) {
+    if (elements[i] == 0) {
+      is_full = false;
+    }
+  }
+  return is_empty;
 }
